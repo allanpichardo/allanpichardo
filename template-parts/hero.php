b25afc1f9a4b421f364333d5a5b6d7144902fd5f
Allan Pichardo <allan.pichardo@gmail.com>
2022-09-30 20:45:51 -0400

    integrated hero fields

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -1,10 +1,15 @@
 <?php
-$version = 'primary'; // primary | secondary
-$title = 'A Clever Title';
-$media_type = 'video'; // image | video
-$url = null;
-$tag = !empty($url) ? 'a' : 'div';
 
+use Portfolio\Theme;
+
+$title      = get_field('title');
+$version    = get_field('version'); // primary | secondary
+$media_type = get_field('media_type'); // image | video
+$url        = get_field('link');
+$tag        = !empty($url) ? 'a' : 'div';
+$image      = get_field('image');
+$video      = get_field('video');
+$notes      = get_field('notes');
 ?>
 <section class="hero page-padding">
     <div class="title-area <?=$version?>">
@@ -12,14 +17,18 @@ $tag = !empty($url) ? 'a' : 'div';
     </div>
     <<?=$tag?> class="media-container" href="#">
         <?php if ($media_type === 'image') : ?>
-            <img src="https://placekitten.com/800/600" alt="Hero Image">
+            <?php if(!empty($image)) {
+                echo Theme::getImageSrcset($image['ID']);
+            } ?>
         <?php elseif ($media_type === 'video') : ?>
-            <video autoplay muted loop playsinline>
-                <source src="<?=sprintf("%s/img/jump1.mp4", get_stylesheet_directory_uri()) ?>" type="video/mp4">
-            </video>
+            <?php if(!empty($video)) {
+                echo Theme::getVideoTag($video['ID']);
+            } ?>
         <?php endif; ?>
     </<?=$tag?>>
-    <div class="notes-area">
-        <p class="sidenote step--1 bold">Some notes 2022</p>
-    </div>
+    <?php if(!empty($notes)): ?>
+        <div class="notes-area">
+            <p class="sidenote step--1 bold"><?=$notes?></p>
+        </div>
+    <?php endif; ?>
 </section>
