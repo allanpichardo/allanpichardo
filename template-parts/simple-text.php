f3ddfeaff506862e4a8c3a83e83b2aee9dbb5723
Allan Pichardo <allan.pichardo@gmail.com>
2022-09-30 21:24:08 -0400

    Integrated simple text

--- template-parts/simple-text.php
+++ template-parts/simple-text.php
@@ -1,10 +1,16 @@
 <?php
+$title = get_sub_field('title');
+$subtitle = get_sub_field('subtitle');
+$body = get_sub_field('body');
 ?>
 <section class="simple-text page-padding">
-	<h2 class="title heading step-2">Lorem Ipsum</h2>
-	<h3 class="subtitle">Education</h3>
+    <?php if(!empty($title)): ?>
+	    <h2 class="title heading step-2"><?=$title?></h2>
+    <?php endif; ?>
+    <?php if(!empty($subtitle)): ?>
+	    <h3 class="subtitle"><?=$subtitle?></h3>
+    <?php endif; ?>
 	<div class="content-section body">
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<?=$body?>
 	</div>
 </section>
