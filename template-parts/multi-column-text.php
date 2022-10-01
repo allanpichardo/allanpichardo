87fb6b55cefc4b4e8e225abf314d74cb7a33e9a5
Allan Pichardo <allan.pichardo@gmail.com>
2022-09-30 21:17:39 -0400

    integrated multi column text

--- template-parts/multi-column-text.php
+++ template-parts/multi-column-text.php
@@ -1,52 +1,23 @@
 <?php
+$title = get_sub_field('title');
+$columns = get_sub_field('columns');
 ?>
 <section class="multi-column-text page-padding">
-	<h2 class="title heading step-2">CV</h2>
+	<h2 class="title heading step-2"><?=$title?></h2>
 	<div class="content-section">
-        <div class="column">
-            <h3 class="subtitle">Education</h3>
-            <ul>
-                <li>
-                    <h4>Bachelor of Science in Computer Science</h4>
-                    <h5>University of Waterloo</h5>
-                    <h5>Sept. 2016 - May 2020</h5>
-                </li>
-                <li>
-                    <h4>Bachelor of Arts in English</h4>
-                    <h5>University of Waterloo</h5>
-                    <h5>Sept. 2015 - May 2019</h5>
-                </li>
-            </ul>
-        </div>
-        <div class="column">
-            <h3 class="subtitle">Education</h3>
-            <ul>
-                <li>
-                    <h4>Bachelor of Science in Computer Science</h4>
-                    <h5>University of Waterloo</h5>
-                    <h5>Sept. 2016 - May 2020</h5>
-                </li>
-                <li>
-                    <h4>Bachelor of Arts in English</h4>
-                    <h5>University of Waterloo</h5>
-                    <h5>Sept. 2015 - May 2019</h5>
-                </li>
-            </ul>
-        </div>
-        <div class="column">
-            <h3 class="subtitle">Education</h3>
-            <ul>
-                <li>
-                    <h4>Bachelor of Science in Computer Science</h4>
-                    <h5>University of Waterloo</h5>
-                    <h5>Sept. 2016 - May 2020</h5>
-                </li>
-                <li>
-                    <h4>Bachelor of Arts in English</h4>
-                    <h5>University of Waterloo</h5>
-                    <h5>Sept. 2015 - May 2019</h5>
-                </li>
-            </ul>
-        </div>
+        <?php if(!empty($columns)): ?>
+            <?php foreach($columns as $column): ?>
+                <?php
+                $subtitle = $column['subtitle'] ?: null;
+                $body = $column['body'];
+                ?>
+                <div class="column">
+                    <?php if(!empty($subtitle)): ?>
+                        <h3 class="subtitle"><?=$subtitle?></h3>
+                    <?php endif; ?>
+                    <?=$body?>
+                </div>
+            <?php endforeach; ?>
+        <?php endif; ?>
     </div>
 </section>
